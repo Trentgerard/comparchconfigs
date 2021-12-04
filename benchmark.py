ee4115839850f9ca46d83d6489c74b0358b8826f
TrentGerard <tag93@psu.edu>
2021-12-03 20:11:05 -0500

    done

--- benchmark.py
+++ benchmark.py
@@ -1,20 +1,34 @@
 import subprocess
-
-benchmarks = [["../bzip2/bzip2_base.i386-m32-gcc42-nn", "../bzip2/dryer.jpg"],["../mcf/mcf_base.i386-m32-gcc42-nn", "../mcf/inp.in"],["../hmmer/hmmer_base.i386-m32-gcc42-nn", "../hmmer/bombesin.hmm"],["../sjeng/sjeng_base.i386-m32-gcc42-nn", "../sjeng/test.txt"],["../milc/milc_base.i386-m32-gcc42-nn", "<", "../milc/su3imp.in"],["../equake/equake_base.pisa_little", "<", "../equake/inp.in", ">", "../equake/inp.out"]]
+import os
+benchmarks = [["./bzip2/bzip2_base.i386-m32-gcc42-nn", "./bzip2/dryer.jpg"],["./mcf/mcf_base.i386-m32-gcc42-nn", "./mcf/inp.in"],["./hmmer/hmmer_base.i386-m32-gcc42-nn", "./hmmer/bombesin.hmm"],["./sjeng/sjeng_base.i386-m32-gcc42-nn", "./sjeng/test.txt"],["./milc/milc_base.i386-m32-gcc42-nn", "<", "./milc/su3imp.in"],["./equake/equake_base.pisa_little", "<", "./equake/inp.in", ">", "./equake/inp.out"]]
 
 benckmarknames = ["bzip_out","mcf_out","hmmer_out","sjeng_out","milc_out","equake_out"]
 
-configs = ["../tmps/tmp1.cfg","../tmps/tmp2.cfg","../tmps/tmp3.cfg","../tmps/tmp4.cfg","../tmps/tmp5.cfg","../tmps/tmp6.cfg","../tmps/tmp7.cfg","../tmps/tmp8.cfg","../tmps/tmp9.cfg","../tmps/tmp10.cfg","../tmps/tmp11.cfg"]
+configs = ["./tmps/a/tmp.cfg","./tmps/b/tmp.cfg","./tmps/c/tmp.cfg","./tmps/d/tmp.cfg","./tmps/e/tmp.cfg","./tmps/f/tmp.cfg","./tmps/g/tmp.cfg","./tmps/h/tmp.cfg","./tmps/i/tmp.cfg","./tmps/j/tmp.cfg","./tmps/k/tmp.cfg"]
 
 basefunc = ["$SIMPLESIM/simplesim-3.0/sim-outorder", "-config"] 
 
 i = 0
 for folder in benckmarknames:
-    subprocess.run(["mkdir",folder])
-    subprocess.run(["cd",folder])
+    #subprocess.run(["mkdir",folder])
+    #os.system("cd "+folder)
+    j = 1
     for config in configs:
-        command = basefunc + [config] + benchmarks[i]
-        subprocess.run(command)
+        #subprocess.run(["cp",config,"tmp.cfg"])
+        pid = os.fork()
+        if pid:
+            os.wait()
+            os.system("echo "+config+" completed")
+            j += 1
+        else:
+            os.system("echo "+config+" in progress")
+            command = ["$SIMPLESIM/simplesim-3.0/sim-outorder", "-config"] + [config] + benchmarks[i]
+            command = " ".join(command)
+            os.system(command)
+            os.system("echo exiting")
+            os.system("cp sim1.out ./"+folder+"/sim"+str(j)+".out")
+            exit()
+            #subprocess.run(["rm","tmp.cfg"])   
     i += 1
-    subprocess.run(["cd",".."])
+    #os.system("cd ..")
     
